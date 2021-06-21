7ac16d8f2bdd46fd2a9c45e6813a36c5cf1353f7
HEEWON <worthwhile_@snu.ac.kr>
2021-06-21 16:26:36 +0900

    map / layout
    맵이랑 틀만 대충 만든 것

--- src/App.js
+++ src/App.js
@@ -1,25 +1,121 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import Map from './Map'
+import './App.css'
+import {Button, ButtonGroup} from "@material-ui/core";
+
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDDP4UaX_T76Q1l4tGOmVebgbSTJhScj6E",
+    authDomain: "venweb-final-project.firebaseapp.com",
+    projectId: "venweb-final-project",
+    storageBucket: "venweb-final-project.appspot.com",
+    messagingSenderId: "484267133169",
+    appId: "1:484267133169:web:0315353ef85ef78efb8150"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+firebase.auth().onAuthStateChanged(function(user){
+    if(user){
+        console.log(user);
+    } else{
+
+    }
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState(null);
+
+    const createAccount =(e)=> {
+        e.preventDefault();
+        firebase.auth().signInWithPopup(firebase.auth())
+
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then((res)=>{
+                console.log(res);
+            })
+            .catch(function(error){
+                console.log(error);
+            })
+    }
+
+    const loginFirebase = () => {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider)
+            .then((result)=>{
+                console.log(result);
+            });
+
+    }
+    // useEffect(()=> {
+    //
+    //     const provider = new firebase.auth.GoogleAuthProvider();
+    //     firebase.auth().signInWithPopup(provider)
+    //         .then((result)=>{
+    //         console.log(result);
+    //         });
+
+        // firebase.auth().signInWithEmailAndPassword("aa@a.com", "aa")
+        //     .then((res) => {
+        //         console.log("signed in!");
+        //         console.log(res);
+        //         firebase.user;
+        //     })
+        //     .catch(function (error) {
+        //         console.log(error);
+        //     });
+
+    // },[]);
+
+
+
+
+
+    return(
+        <div> <h1 id={"title"}>코로나 정보</h1>
+
+            <div className={"topLevel"}>
+
+
+                <div className={"leftSide"}>
+                    <div id={"covidConfirmed"}>
+                        확진자 정보 미구현
+                    </div>
+                    <div className={"vaccineInfo"}>
+                        <div id={"map"}>
+                            지도
+                            <Map/>
+                        </div>
+                        <div id={"vaccinationLocation"}>
+                            백신 접종처 정보 미구현
+                        </div>
+                    </div>
+                </div>
+
+                <div className={"rightSide"}>
+                    <div id={"login"}>
+                        {/*<form onSubmit={createAccount}>*/}
+                        {/*    <input name={"email"} onChange={e=>setEmail(e.target.value)}/>*/}
+                        {/*    <input name={"password"} onChange={e=>setPassword(e.target.value)}/>*/}
+                        {/*    <input type={"submit"} value={"submit"}/>*/}
+                        {/*</form>*/}
+                        <button onClick={loginFirebase}>로그인</button>
+                    </div>
+                    <div id={"news"}>
+                        뉴스 미구현
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
 }
 
 export default App;
+
+
---
+++ src/Map.js
@@ -0,0 +1,95 @@
+/*global kakao*/
+import React, { useEffect } from 'react'
+
+const Map = ({ searchPlace }) =>{
+
+    const places = new kakao.maps.services.Places();
+    const callback = function(result, status) {
+        if (status === kakao.maps.services.Status.OK) {
+            console.log(result);
+        }
+    };
+
+    places.keywordSearch('판교 치킨', callback);
+
+    useEffect(()=>{
+        const container = document.getElementById('map');
+        const options = {
+            center: new kakao.maps.LatLng(37.4824761,126.9519317),
+            level: 3
+        };
+        const map = new kakao.maps.Map(container, options);
+
+        const markerPosition  = new kakao.maps.LatLng(37.4824761,126.9519317);
+        const marker = new kakao.maps.Marker({
+            position: markerPosition
+        });
+
+        const ps = new kakao.maps.services.Places();
+
+        ps.keywordSearch('입력 값', placesSearchCB);
+
+        function placesSearchCB (data, status, pagination) {
+            if (status === kakao.maps.services.Status.OK) {
+
+                let bounds = new kakao.maps.LatLngBounds();
+
+                for (let i=0; i<data.length; i++) {
+                    displayMarker(data[i]);
+                    bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+                }
+
+                map.setBounds(bounds);
+            }
+        }
+
+
+
+        function displayMarker(place) {
+            let marker = new kakao.maps.Marker({
+                map: map,
+                position: new kakao.maps.LatLng(place.y, place.x)
+            });
+        }
+
+// 마커를 클릭했을 때 마커 위에 표시할 인포윈도우를 생성합니다
+        let iwContent = '<div style="padding:5px;">Hello World!</div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+            iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
+
+        // 인포윈도우를 생성합니다
+        let infowindow = new kakao.maps.InfoWindow({
+            content : iwContent,
+            removable : iwRemoveable
+        });
+
+        // 마커에 클릭이벤트를 등록합니다
+        kakao.maps.event.addListener(marker, 'click', function() {
+            // 마커 위에 인포윈도우를 표시합니다
+            infowindow.open(map, marker);
+        });
+
+        // 마커에 마우스오버 이벤트를 등록합니다
+        kakao.maps.event.addListener(marker, 'mouseover', function() {
+            // 마커에 마우스오버 이벤트가 발생하면 인포윈도우를 마커위에 표시합니다
+            infowindow.open(map, marker);
+        });
+
+        // 마커에 마우스아웃 이벤트를 등록합니다
+        kakao.maps.event.addListener(marker, 'mouseout', function() {
+            // 마커에 마우스아웃 이벤트가 발생하면 인포윈도우를 제거합니다
+            infowindow.close();
+        });
+
+
+
+        marker.setMap(map);},[searchPlace])
+
+
+    return (
+        <div>
+            <div id="map" style={{width:"500px", height:"500px"}}></div>
+        </div>
+    )
+}
+
+export default Map;
