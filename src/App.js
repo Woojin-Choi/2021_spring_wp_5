c4c76dc7606c877b6cbd94637384f400a9d19aa7
HEEWON <worthwhile_@snu.ac.kr>
2021-06-22 09:04:24 +0900

    로그인 오류 정정, 즐겨찾기 기능 추가
    -

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, {useState, useEffect} from 'react'
 import Map from './Map'
 import './App.css'
-import {Button, ButtonGroup} from "@material-ui/core";
+import {Button, ButtonGroup, Box} from "@material-ui/core";
+import AddCircleOutlineRoundedIcon from '@material-ui/icons/AddCircleOutlineRounded';
 
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
 import {getVLocation} from './Api'
-// import {showLocation} from "./showLocation";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDDP4UaX_T76Q1l4tGOmVebgbSTJhScj6E",
@@ -18,6 +18,7 @@ const firebaseConfig = {
     messagingSenderId: "484267133169",
     appId: "1:484267133169:web:0315353ef85ef78efb8150"
 };
+
 
 
 if (!firebase.apps.length) {
@@ -42,39 +43,46 @@ function App() {
     const [selectedLoc, setSelectedLoc] = useState(null);
     const [locInfo, setLocInfo] = useState({});
     const [user, setUser] = useState(null);
+    const [favoritePanel, setFavoritePanel] = useState(false);
+    const [favLoc, setFavLoc] = useState([]);
 
     const db = firebase.firestore();
 
-    if(!vLocations) {
-
-        console.log("되나")
-        getVLocation()
-        .then(_info => {
-            console.log(_info, "얘는");
-            setVLocations(_info)
-        })
-
-        console.log(vLocations);
-        console.log("출력");
+    if(localStorage.LOGIN_KEY) {
+        if(!user) setUser(localStorage.LOGIN_KEY)
     }
 
-    const createAccount =(e)=> {
-        e.preventDefault();
+    // if(!vLocations) {
+    //
+    //     console.log("되나")
+    //     getVLocation()
+    //     .then(_info => {
+    //         console.log(_info, "얘는");
+    //         setVLocations(_info)
+    //     })
+    //
+    //     console.log(vLocations);
+    //     console.log("출력");
+    // }
 
-        firebase.auth().createUserWithEmailAndPassword(email, password)
-            .then((res)=>{
-                console.log(res);
-            })
-            .catch(function(error){
-                console.log(error);
-            })
-    }
+    // const createAccount =(e)=> {
+    //     e.preventDefault();
+    //
+    //     firebase.auth().createUserWithEmailAndPassword(email, password)
+    //         .then((res)=>{
+    //             console.log(res);
+    //         })
+    //         .catch(function(error){
+    //             console.log(error);
+    //         })
+    // }
 
     const loginFirebase = () => {
         const provider = new firebase.auth.GoogleAuthProvider();
         firebase.auth().signInWithPopup(provider)
             .then((result)=>{
-                setUser(result.user.email);
+                localStorage.setItem("LOGIN_KEY", result.user.email);
+                setUser(localStorage.LOGIN_KEY)
             })
             .catch((error)=>{
                 const errorCode = error.code;
@@ -86,11 +94,18 @@ function App() {
     const logoutFirebase = () => {
         firebase.auth().signOut()
             .then((result)=>{
+                localStorage.clear();
+                setFavoritePanel(false);
                 setUser(null);
+
                 console.log(result);
             })
+            .catch((error)=>{
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorMessage);
+            })
     }
-
 
     const locationCheck = (e) => {
         setSelectedLoc(e.orgZipaddr);
@@ -103,19 +118,53 @@ function App() {
         setLocInfo(_locInfo)
     }
 
+    const favoriteAdd = (elem) => {
+        if(user) {
+            elem.userId=user;
+            dbAdd("favoriteOrg",elem)}
+        else {
+            alert("로그인 이후 사용가능합니다")
+        }
+    }
+
+    const getFavorite = async () => {
+
+
+        // setFavLoc(db.collection.get("favoriteOrg"))
+
+    }
+
+    const favoriteCheck = async (e) =>{
+        if(e==="u") setFavoritePanel(false);
+        else {
+            setFavoritePanel(true);
+            const myFav = [];
+            db.collection("favoriteOrg").get()
+            .then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    if(doc.data().userId === user) {
+                        myFav.push(doc.data())
+                    }
+                })
+
+                setFavLoc(myFav)
+                console.log(myFav)
+            })
+
+        }
+    }
+
     useEffect(()=>{
         getVLocation()
             .then(_info => {
                 setVLocations(_info)
             })
     },[])
-    // const DBFUNC1 = () => { // db에 추가하는 함수
-    //    db.collection("datas").add({
-    //         first: "1",
-    //         last: "2",
-    //         born: 1995
-    //     })
-    // }
+
+
+    const dbAdd = (category, data) => { // db에 추가하는 함수
+       db.collection(category).add(data);
+    }
     //
     // //https://firebase.google.com/docs/firestore/manage-data/delete-data?hl=ko 삭제는 여기 참고
     //
@@ -155,7 +204,7 @@ function App() {
             </header>
 
             <body>
-                <div className={"topLevel"}>
+                <Box className={"topLevel"}>
                     <div className={"leftSide"}>
                         <div id={"covidConfirmed"}>
                             확진자 정보 미구현
@@ -165,23 +214,52 @@ function App() {
                                 <Map selectedLoc={selectedLoc} locInfo={locInfo}/>
                             </div>
                             <div id={"vaccinationLocation"}>
-                                <h2>백신 접종처(가나다 순)</h2>
+
                                 <div className="location">
                                     {
-                                        vLocations.slice(0).sort(function(a,b) {
-                                            return a.orgnm < b.orgnm ? -1 : a.orgnm > b.orgnm ? 1: 0;
-                                        }).map(elem => {
-                                            return (
-                                                <div className="locationBox" key={elem.orgcd}>
-                                                    <ul>
-                                                        <li>기관명: {elem.orgnm}</li>
-                                                        <li>전화번호: {elem.orgTlno}</li>
-                                                        <li>주소: {elem.orgZipaddr}</li>
-                                                        <li>당일 휴무여부: {elem.hldyYn}</li>
-                                                        <Button id="locationButton" variant="outlined" color="primary" onClick={()=>locationCheck(elem)}>위치 확인</Button>
-                                                    </ul>
-                                                </div>);
-                                        })
+                                        favoritePanel?
+                                            <div>
+                                                <h2>즐겨찾는 병원</h2>
+                                                {
+                                                    favLoc.slice(0).sort(function(a,b) {
+                                                        return a.orgnm < b.orgnm ? -1 : a.orgnm > b.orgnm ? 1: 0;
+                                                    }).map(elem => {
+                                                        return (
+                                                            <div className="locationBox" key={elem.orgcd}>
+                                                                <ul>
+                                                                    <li>기관명: {elem.orgnm}</li>
+                                                                    <li>전화번호: {elem.orgTlno}</li>
+                                                                    <li>주소: {elem.orgZipaddr}</li>
+                                                                    <li>당일 휴무여부: {elem.hldyYn}</li>
+                                                                </ul>
+                                                                    <Button id="locationButton" variant="outlined" color="primary" onClick={()=>locationCheck(elem)}>위치 확인</Button>
+                                                            </div>);
+                                                    })
+                                                }
+                                            </div>
+                                            :
+                                            <div>
+                                                <h2>백신 접종처 (가나다 순)</h2>
+                                                {
+                                                    vLocations.slice(0).sort(function(a,b) {
+                                                        return a.orgnm < b.orgnm ? -1 : a.orgnm > b.orgnm ? 1: 0;
+                                                     }).map(elem => {
+                                                         return (
+                                                            <div className="locationBox" key={elem.orgcd}>
+                                                                <ul>
+                                                                    <li>기관명: {elem.orgnm}</li>
+                                                                    <li>전화번호: {elem.orgTlno}</li>
+                                                                    <li>주소: {elem.orgZipaddr}</li>
+                                                                    <li>당일 휴무여부: {elem.hldyYn}</li>
+                                                                </ul>
+                                                                <ButtonGroup color="primary" aria-label="outlined primary button group">
+                                                                    <Button id="locationButton" onClick={()=>locationCheck(elem)}>위치 확인</Button>
+                                                                    <Button id="favoriteButton" size="small" startIcon={<AddCircleOutlineRoundedIcon/>} onClick={()=>favoriteAdd(elem)}>즐겨찾기 추기</Button>
+                                                                </ButtonGroup>
+                                                            </div>);
+                                                    })
+                                                }
+                                            </div>
                                     }
                                 </div>
                             </div>
@@ -195,12 +273,19 @@ function App() {
                             {/*    <input name={"password"} onChange={e=>setPassword(e.target.value)}/>*/}
                             {/*    <input type={"submit"} value={"submit"}/>*/}
                             {/*</form>*/}
-                            {user ?
-                                <Button id="loginButton" onClick={logoutFirebase} variant="contained"
-                                        color="primary">로그아웃</Button>
+                            {localStorage.getItem("LOGIN_KEY") ?
+                                <div id={"loggedIn"}>
+                                    <span>{localStorage.getItem("LOGIN_KEY")}님 반갑습니다!</span>
+                                    <Button id="loginButton" onClick={logoutFirebase} variant="contained"
+                                            color="primary">로그아웃</Button>
+                                    <Button id="myFavorite" onClick={()=>favoritePanel?favoriteCheck("u"):favoriteCheck("c")} variant="contained"
+                                                    color="primary">{favoritePanel?'전체 목록 보기':'즐겨찾는 병원보기'}</Button>
+
+
+                                </div>
                                 :
                                 <Button id="loginButton" onClick={loginFirebase} variant="contained"
-                                color="primary">로그인</Button>
+                                        color="primary">구글아이디로 로그인</Button>
                             }
                         </div>
                         <div id={"news"}>
@@ -208,7 +293,7 @@ function App() {
                         </div>
                     </div>
 
-                </div>
+                </Box>
             </body>
         </div>
     )
