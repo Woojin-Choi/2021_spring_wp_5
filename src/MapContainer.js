35d0a43a6408e9903bea2f9a746bb91ee35763e0
HEEWON <worthwhile_@snu.ac.kr>
2021-06-23 14:36:44 +0900

    var->let

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -5,7 +5,7 @@ const { kakao } = window
 const MapContainer = ({ searchPlace }) => {
 
     useEffect(() => {
-        var infowindow = new kakao.maps.InfoWindow({ zIndex: 1 })
+        let infowindow = new kakao.maps.InfoWindow({ zIndex: 1 })
         const container = document.getElementById('myMap')
         const options = {
             center: new kakao.maps.LatLng(33.450701, 126.570667),
