16cbf6676d4a728ac8f1fa7d09f3d1f95d1124c0
HEEWON <worthwhile_@snu.ac.kr>
2021-06-23 09:41:56 +0900

    css 수정

--- src/VaccineInfo.js
+++ src/VaccineInfo.js
@@ -6,18 +6,13 @@ import React, {useEffect, useState} from "react";
 import {getVLocation} from "./Api";
 import firebase from "firebase";
 
-
-
 export default function VaccineInfo(props) {
 
     const [vLocations, setVLocations] = useState([]);
     const [selectedLoc, setSelectedLoc] = useState(null);
     const [locInfo, setLocInfo] = useState({});
-    const db = firebase.firestore();
 
-    const dbAdd = (category, data) => { // db에 추가하는 함수
-        db.collection(category).add(data);
-    }
+    const db = firebase.firestore();
 
     const locationCheck = (e) => {
         setSelectedLoc(e.orgZipaddr);
@@ -30,9 +25,7 @@ export default function VaccineInfo(props) {
         setLocInfo(_locInfo)
     }
 
-
-
-    const favoriteAdd = (elem) => {
+    const addFavLoc = (elem) => {
         if(props.user) {
             elem.userId = props.user;
             const myFav = [];
@@ -40,9 +33,7 @@ export default function VaccineInfo(props) {
             db.collection("favoriteOrg").get()
                 .then((querySnapshot) => {
                     querySnapshot.forEach((doc) => {
-                        console.log(doc,"doc이 뭐지")
                         if (doc.data().userId === props.user) {
-                            console.log(doc.data(),"doc.date는 뭐지")
                             myFav.push(doc.data())
                         }
                     })
@@ -61,21 +52,7 @@ export default function VaccineInfo(props) {
         else {alert("로그인 이후 사용가능합니다")}
     }
 
-    const updateFavLoc = () => {
-        const myFav = [];
-        db.collection("favoriteOrg").get()
-            .then((querySnapshot) => {
-                querySnapshot.forEach((doc) => {
-                    if(doc.data().userId === props.user) {
-                        myFav.push(doc.data())
-                    }
-                })
-
-                props.setFavLoc(myFav)
-            })
-    }
-
-    const favoriteDel = (elem) => {
+    const delFavLoc = (elem) => {
         if(props.user) {
             elem.userId = props.user;
             db.collection("favoriteOrg").get()
@@ -99,7 +76,18 @@ export default function VaccineInfo(props) {
         else {alert("로그인 이후 사용가능합니다")}
     }
 
-
+    const updateFavLoc = () => {
+        const myFav = [];
+        db.collection("favoriteOrg").get()
+            .then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    if(doc.data().userId === props.user) {
+                        myFav.push(doc.data())
+                    }
+                })
+                props.setFavLoc(myFav)
+            })
+    }
 
     useEffect(()=>{
         getVLocation()
@@ -127,14 +115,14 @@ export default function VaccineInfo(props) {
                                             return a.orgnm < b.orgnm ? -1 : a.orgnm > b.orgnm ? 1 : 0;
                                         }).map(elem => {
                                             return (
-                                                <div className="LocationBox" key={elem.orgcd}>
+                                                <div className="locationBox" key={elem.orgcd}>
                                                     <ul className={"favInfo"}>
                                                         <li>기관명: {elem.orgnm}</li>
                                                         <li>전화번호: {elem.orgTlno}</li>
                                                         <li>주소: {elem.orgZipaddr}</li>
                                                         <li>당일 휴무여부: {elem.hldyYn}</li>
                                                     </ul>
-                                                    <ButtonGroup className="BtnGroup" color="primary"
+                                                    <ButtonGroup className="btnGroup" color="primary"
                                                                  aria-label="outlined primary button group">
                                                         <Button class="locationButton" variant="outlined"
                                                                 color="primary"
@@ -142,8 +130,7 @@ export default function VaccineInfo(props) {
                                                             확인</Button>
                                                         <Button id="favoriteDelButton" size="small"
                                                                 startIcon={<AddCircleOutlineRoundedIcon/>}
-                                                                onClick={() => favoriteDel(elem)}>즐겨찾기
-                                                            삭제</Button>
+                                                                onClick={() => delFavLoc(elem)}>즐겨찾기 삭제</Button>
                                                     </ButtonGroup>
                                                 </div>);
                                         })
@@ -156,28 +143,27 @@ export default function VaccineInfo(props) {
                                     <h2>백신 접종처 (가나다 순)</h2>
                                 </div>
                                 <div className={"locationGroup"}>
-                                {
-                                    vLocations.slice(0).sort(function (a, b) {
-                                        return a.orgnm < b.orgnm ? -1 : a.orgnm > b.orgnm ? 1 : 0;
-                                    }).map(elem => {
-                                        return (
-                                            <div className="locationBox" key={elem.orgcd}>
-                                                <ul className={"favInfo"}>
-                                                    <li>기관명: {elem.orgnm}</li>
-                                                    <li>전화번호: {elem.orgTlno}</li>
-                                                    <li>주소: {elem.orgZipaddr}</li>
-                                                    <li>당일 휴무여부: {elem.hldyYn}</li>
-                                                </ul>
-                                                <ButtonGroup className="BtnGroup" color="primary" aria-label="outlined primary button group">
-                                                    <Button class="locationButton" onClick={() => locationCheck(elem)}>위치
-                                                        확인</Button>
-                                                    <Button id="favoriteAddButton" size="small"
-                                                            startIcon={<AddCircleOutlineRoundedIcon/>}
-                                                            onClick={() => favoriteAdd(elem)}>즐겨찾기 추가</Button>
-                                                </ButtonGroup>
-                                            </div>);
-                                    })
-                                }
+                                    {
+                                        vLocations.slice(0).sort(function (a, b) {
+                                            return a.orgnm < b.orgnm ? -1 : a.orgnm > b.orgnm ? 1 : 0;
+                                        }).map(elem => {
+                                            return (
+                                                <div className="locationBox" key={elem.orgcd}>
+                                                    <ul className={"favInfo"}>
+                                                        <li>기관명: {elem.orgnm}</li>
+                                                        <li>전화번호: {elem.orgTlno}</li>
+                                                        <li>주소: {elem.orgZipaddr}</li>
+                                                        <li>당일 휴무여부: {elem.hldyYn}</li>
+                                                    </ul>
+                                                    <ButtonGroup className="btnGroup" color="primary" aria-label="outlined primary button group">
+                                                        <Button class="locationButton" onClick={() => locationCheck(elem)}>위치 확인</Button>
+                                                        <Button id="favoriteAddButton" size="small"
+                                                                startIcon={<AddCircleOutlineRoundedIcon/>}
+                                                                onClick={() => addFavLoc(elem)}>즐겨찾기 추가</Button>
+                                                    </ButtonGroup>
+                                                </div>);
+                                        })
+                                    }
                                 </div>
                             </div>
                     }
