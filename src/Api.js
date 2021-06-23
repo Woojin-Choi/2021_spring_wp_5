84c49e5af08266312ec90568116e9abdc13f8e87
HEEWON <worthwhile_@snu.ac.kr>
2021-06-23 10:38:05 +0900

    cors 주소 변경 / 지도 에러 수정

--- src/Api.js
+++ src/Api.js
@@ -56,7 +56,7 @@ const getCovidStatus = async (extraHeaders={}) => {
   const d = new Date().getDate().toString()
   const date = m<9? y+'0'+(m+1).toString()+d : y+(m+1).toString()+d;
     const AUTH_KEY = '%2F7IOSPMoyPtgQMbjrMUuHMSuO2IjSAO2gxgrKYU8zb5hPcSqXo7Z%2BLiHtuglidhA65F9qotVT7b4rocoZLXmCg%3D%3D'
-  const defaultUrl = 'https://cors-anywhere.herokuapp.com/http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson'
+  const defaultUrl = 'https://snu-cors.herokuapp.com/http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson'
   const query = `serviceKey=${AUTH_KEY}&pageNo=1&numOfRows=30&startCreate_dt=${date}&endCreateDt=${date}&&_type=json`
   const res = await fetch(`${defaultUrl}?${query}`, {
     method: 'GET',
